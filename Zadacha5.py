c6c4b19d483530cbed0351e8043908f89018eb76
AleksandraM <musatova.aleksandra@mail.ru>
2022-11-25 12:19:09 +0300

    Задачи решены

---
+++ Zadacha5.py
@@ -0,0 +1,12 @@
+# 5. Реализуйте алгоритм перемешивания списка.
+
+import random
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i in range(len(lst)):
+    a = random.randint(0,7)
+    b = lst[i]
+    lst[i] = lst[a]
+    lst[a] = b
+print(lst)
